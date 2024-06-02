3cbea9a670d2eb36a228acececf84382d9ee4a56
不明 <Bio.Giokabaneri@Gmail.com>
2024-06-03 01:27:37 +0700

    Update index.tsx

--- src/app/sections/falling-caps/index.tsx
+++ src/app/sections/falling-caps/index.tsx
@@ -9,46 +9,6 @@ import s from './caps.module.scss'
 
 export const FallingCaps = () => {
   const caps: CSSProperties[] = [
-    {
-      transform: 'translate(0, 0) scale(0.85) rotate(-25deg)',
-      top: '7%',
-      left: '3%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.75) rotate(-22deg)',
-      top: '18%',
-      right: '12%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.55) rotate(-28deg)',
-      bottom: '18%',
-      right: '35%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.65) rotate(22deg)',
-      top: '2%',
-      right: '2%'
-    },
-    {
-      transform: 'translate(0, 0) rotate(22deg)',
-      top: '10%',
-      left: '35%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.7) rotate(27deg)',
-      bottom: '7%',
-      left: '1%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.72) rotate(22deg)',
-      bottom: '1%',
-      left: '18%'
-    },
-    {
-      transform: 'translate(0, 0) scale(0.85) rotate(-8deg)',
-      bottom: '1%',
-      right: '1%'
-    }
   ]
   return (
     <Container className={s.container}>
